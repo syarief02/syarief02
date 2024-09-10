edde4c3fc0654654b78c045532efaa58e283cbd6
Syarief Azman <105992338+syarief02@users.noreply.github.com>
2024-09-10 22:08:26 +0800

    fdsfd

--- LAST_UPDATED.c
+++ LAST_UPDATED.c
@@ -32,38 +32,3 @@ int a24 = 1;
 
 return 0;
 }
-
-// Updated on 2024-09-10
-// Updated on 2024-09-10
-#include <stdio.h>
-
-int main(void)
-{
-    int a1 = 1;
-int a2 = 1;
-int a3 = 1;
-int a4 = 1;
-int a5 = 1;
-int a6 = 1;
-int a7 = 1;
-int a8 = 1;
-int a9 = 1;
-int a10 = 1;
-int a11 = 1;
-int a12 = 1;
-int a13 = 1;
-int a14 = 1;
-int a15 = 1;
-int a16 = 1;
-int a17 = 1;
-int a18 = 1;
-int a19 = 1;
-int a20 = 1;
-int a21 = 1;
-int a22 = 1;
-int a23 = 1;
-int a24 = 1;
-
-return 0;
-}
-
